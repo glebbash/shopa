7a109c30496a7b65e3dfcff20bfac1ba0acceaf7
glebbash <glebbash@gmail.com>
2022-12-10 12:52:05 +0000

    feat: update login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,27 +6,26 @@ import Button from "@mui/material/Button";
 import { supabase } from "../lib/supabase";
 
 export function LoginPage() {
+  const handleLogin = async () => {
+    await supabase.signIn();
+  };
+
   return (
     <Box
       sx={{
         p: 4,
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Avatar src="/shopa.png" sx={{ width: 72, height: 72 }} />
-      <Typography>Shopa</Typography>
-      <LoginButton />
+      <Avatar src="/shopa.png" sx={{ width: 128, height: 128 }} />
+      <Typography variant="h1">Shopa</Typography>
+      <Button sx={{ m: 4 }} variant="contained" onClick={handleLogin}>
+        Log in
+      </Button>
     </Box>
   );
-}
-
-function LoginButton() {
-  const handleLogin = async () => {
-    await supabase.signIn();
-  };
-
-  return <Button onClick={handleLogin}>Log in</Button>;
 }

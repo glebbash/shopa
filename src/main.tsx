a71508d2e2bbfdd3879f9eec91ce10e94235e1e0
glebbash <glebbash@gmail.com>
2022-12-10 13:51:25 +0000

    chore: improve global overscroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
 
 import { MyListsPage } from "./pages/MainPage";
 import { ShoppingListPage } from "./pages/ShoppingListPage";
